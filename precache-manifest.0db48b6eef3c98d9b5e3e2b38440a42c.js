266d794abeff16d5bba8008a0276b7d911fc3110
Adeyeye Olushola <adeyeyeolushola977@gmail.com>
2020-07-21 22:28:50 +0100

    Updates

---
+++ precache-manifest.0db48b6eef3c98d9b5e3e2b38440a42c.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b38c54499c49ae63bc779ba85c3497",
+    "url": "/Olushola977/BaseAfrique-React/index.html"
+  },
+  {
+    "revision": "4bb374dcb3e2bde99c25",
+    "url": "/Olushola977/BaseAfrique-React/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "5723e699196c85998935",
+    "url": "/Olushola977/BaseAfrique-React/static/css/main.84200dab.chunk.css"
+  },
+  {
+    "revision": "4bb374dcb3e2bde99c25",
+    "url": "/Olushola977/BaseAfrique-React/static/js/2.d78a743b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Olushola977/BaseAfrique-React/static/js/2.d78a743b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5723e699196c85998935",
+    "url": "/Olushola977/BaseAfrique-React/static/js/main.b2ce01f6.chunk.js"
+  },
+  {
+    "revision": "bdb387b1a9c3e75ae27d",
+    "url": "/Olushola977/BaseAfrique-React/static/js/runtime-main.eba90268.js"
+  },
+  {
+    "revision": "a851cb62e92005f6530bcf49682e1895",
+    "url": "/Olushola977/BaseAfrique-React/static/media/banner-man.a851cb62.png"
+  },
+  {
+    "revision": "dc3ead4fc9217a158e159817e6980b2b",
+    "url": "/Olushola977/BaseAfrique-React/static/media/banner-woman.dc3ead4f.png"
+  },
+  {
+    "revision": "34d058d6f78f4486494fbac64c8364f0",
+    "url": "/Olushola977/BaseAfrique-React/static/media/banner-woman2.34d058d6.png"
+  },
+  {
+    "revision": "e7b00c6248b1f3bdeed5af3e90f48f61",
+    "url": "/Olushola977/BaseAfrique-React/static/media/ben-img.e7b00c62.png"
+  },
+  {
+    "revision": "df6116efbb5bceb66c90208004de6fa6",
+    "url": "/Olushola977/BaseAfrique-React/static/media/business-and-finance.df6116ef.svg"
+  },
+  {
+    "revision": "f11cd21b43d3c89c2ec68c511cef80a2",
+    "url": "/Olushola977/BaseAfrique-React/static/media/card-design.f11cd21b.svg"
+  },
+  {
+    "revision": "7ff55085673b9dc815a84de796b4c1ff",
+    "url": "/Olushola977/BaseAfrique-React/static/media/cisco.7ff55085.svg"
+  },
+  {
+    "revision": "f61fac0ba91755fce5f57da748f2db78",
+    "url": "/Olushola977/BaseAfrique-React/static/media/cloud.f61fac0b.svg"
+  },
+  {
+    "revision": "099994ffa827ca726816d96f33c13677",
+    "url": "/Olushola977/BaseAfrique-React/static/media/co-working-img.099994ff.png"
+  },
+  {
+    "revision": "57d0edf77593f94d547ed6837120fb3b",
+    "url": "/Olushola977/BaseAfrique-React/static/media/coins-stacks-with-arrow-upwards.57d0edf7.svg"
+  },
+  {
+    "revision": "efe8a327e4e6e5cc7973b649cfbd4ad1",
+    "url": "/Olushola977/BaseAfrique-React/static/media/coke.efe8a327.png"
+  },
+  {
+    "revision": "f12a880513ea7905618b5524bd6fbda2",
+    "url": "/Olushola977/BaseAfrique-React/static/media/commvault-logo.f12a8805.svg"
+  },
+  {
+    "revision": "a5acfc10a352f308b7601aac74fa02d5",
+    "url": "/Olushola977/BaseAfrique-React/static/media/copyright-line.a5acfc10.svg"
+  },
+  {
+    "revision": "9a2b3e7a516dbe0bebdd23c27e9f76c0",
+    "url": "/Olushola977/BaseAfrique-React/static/media/cyber-security.9a2b3e7a.svg"
+  },
+  {
+    "revision": "69a1050eb4e0485f32ee511c19064ed2",
+    "url": "/Olushola977/BaseAfrique-React/static/media/cyberarklogo.69a1050e.svg"
+  },
+  {
+    "revision": "05360219c001a31436886ee03f91e9ce",
+    "url": "/Olushola977/BaseAfrique-React/static/media/dell.05360219.svg"
+  },
+  {
+    "revision": "defccf1921eaf856ef1d0d7c884d1468",
+    "url": "/Olushola977/BaseAfrique-React/static/media/directors.defccf19.png"
+  },
+  {
+    "revision": "a80ded78e4827d1dfd49dae256c3d251",
+    "url": "/Olushola977/BaseAfrique-React/static/media/dot.a80ded78.svg"
+  },
+  {
+    "revision": "6bb0f7116e68106a7da6a75116661d05",
+    "url": "/Olushola977/BaseAfrique-React/static/media/eventImg.6bb0f711.png"
+  },
+  {
+    "revision": "cef84e9581d5599efe2311e6044dea21",
+    "url": "/Olushola977/BaseAfrique-React/static/media/fashion-show.cef84e95.png"
+  },
+  {
+    "revision": "8fbe6343a90d4e2f08a9f9a8e3eca1af",
+    "url": "/Olushola977/BaseAfrique-React/static/media/fireeye-logo.8fbe6343.svg"
+  },
+  {
+    "revision": "70ec9ea8b50447f401b6c7e8d35a2d65",
+    "url": "/Olushola977/BaseAfrique-React/static/media/goal.70ec9ea8.svg"
+  },
+  {
+    "revision": "45b89555dcd691ba581a2085e3184255",
+    "url": "/Olushola977/BaseAfrique-React/static/media/happy-man.45b89555.png"
+  },
+  {
+    "revision": "a270aa0eed66bce577a08c354883b34b",
+    "url": "/Olushola977/BaseAfrique-React/static/media/hewlett-packard-logo.a270aa0e.svg"
+  },
+  {
+    "revision": "459441a6ed14ce3cdc6f97458fdc702a",
+    "url": "/Olushola977/BaseAfrique-React/static/media/hp-2.459441a6.svg"
+  },
+  {
+    "revision": "bbcebb28c65c54c082f2147d7adf09ea",
+    "url": "/Olushola977/BaseAfrique-React/static/media/ibm.bbcebb28.svg"
+  },
+  {
+    "revision": "1d6e82172626ccde444cef235eb0c3f8",
+    "url": "/Olushola977/BaseAfrique-React/static/media/imperva.1d6e8217.svg"
+  },
+  {
+    "revision": "f9e9c0fafb5c75ba497d81754d046450",
+    "url": "/Olushola977/BaseAfrique-React/static/media/info-woman.f9e9c0fa.png"
+  },
+  {
+    "revision": "b0f64031d138013d21a4712e43295118",
+    "url": "/Olushola977/BaseAfrique-React/static/media/intellinx.b0f64031.svg"
+  },
+  {
+    "revision": "095d47c13d9e2b3c97874f2386909d63",
+    "url": "/Olushola977/BaseAfrique-React/static/media/internet.095d47c1.svg"
+  },
+  {
+    "revision": "d5dd5d055f763a702e944ee060e68d84",
+    "url": "/Olushola977/BaseAfrique-React/static/media/laughing-woman.d5dd5d05.png"
+  },
+  {
+    "revision": "6becfe1ea03b0237cae4841c11d1cc02",
+    "url": "/Olushola977/BaseAfrique-React/static/media/mail-line.6becfe1e.svg"
+  },
+  {
+    "revision": "8c2ed5ab085fdb2958cb6a9a9f393d74",
+    "url": "/Olushola977/BaseAfrique-React/static/media/map-pin-line.8c2ed5ab.svg"
+  },
+  {
+    "revision": "bf6b7bea9812e3f98d85e55f4b190678",
+    "url": "/Olushola977/BaseAfrique-React/static/media/mary.bf6b7bea.png"
+  },
+  {
+    "revision": "0569fa30950c3c75bd7bd94a5126c13e",
+    "url": "/Olushola977/BaseAfrique-React/static/media/menu.0569fa30.svg"
+  },
+  {
+    "revision": "c9c96603de56963007baa92e7e12aad3",
+    "url": "/Olushola977/BaseAfrique-React/static/media/microsoft.c9c96603.svg"
+  },
+  {
+    "revision": "e61432ae89b7c91f8695447bd8e10073",
+    "url": "/Olushola977/BaseAfrique-React/static/media/observeit.e61432ae.svg"
+  },
+  {
+    "revision": "1547f6903e6d150495d497c552266b05",
+    "url": "/Olushola977/BaseAfrique-React/static/media/optimization.1547f690.svg"
+  },
+  {
+    "revision": "8b88ded90c1b556a798bf60d8e301951",
+    "url": "/Olushola977/BaseAfrique-React/static/media/perception.8b88ded9.svg"
+  },
+  {
+    "revision": "7269b6e71331b33c4e8e170efc27a7f2",
+    "url": "/Olushola977/BaseAfrique-React/static/media/phone-line.7269b6e7.svg"
+  },
+  {
+    "revision": "31ff488511bf115c958cd557384d52dd",
+    "url": "/Olushola977/BaseAfrique-React/static/media/pivotal.31ff4885.svg"
+  },
+  {
+    "revision": "6e182b871fc3e60c96c9180560487f9d",
+    "url": "/Olushola977/BaseAfrique-React/static/media/route.6e182b87.svg"
+  },
+  {
+    "revision": "1d9e0fa9ace903c144129a262f998a8c",
+    "url": "/Olushola977/BaseAfrique-React/static/media/success.1d9e0fa9.svg"
+  },
+  {
+    "revision": "4a409ebe82be37f9fa0e356919562dec",
+    "url": "/Olushola977/BaseAfrique-React/static/media/symantec.4a409ebe.svg"
+  },
+  {
+    "revision": "3500da81a7c9a4472fce5790b88d4820",
+    "url": "/Olushola977/BaseAfrique-React/static/media/thomas-ordahl.3500da81.png"
+  },
+  {
+    "revision": "7591b106a0c4d555d09b82ff8f86383c",
+    "url": "/Olushola977/BaseAfrique-React/static/media/vmware-1.7591b106.svg"
+  },
+  {
+    "revision": "5ab68d553d9b79143fd498cda01ed6f9",
+    "url": "/Olushola977/BaseAfrique-React/static/media/vmware.5ab68d55.svg"
+  }
+]);
